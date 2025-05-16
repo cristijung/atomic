f55ecbcd37072bc49fa26df0b45b9f4be1fde2ae
CristinaJung <cristijung@gmail.com>
2025-05-16 17:21:34 -0300

    barnav

--- app/components/atoms/textMenu/TextMenu.tsx
+++ app/components/atoms/textMenu/TextMenu.tsx
@@ -1,11 +1,25 @@
+import Link, { LinkProps } from "next/link";
 import React from "react";
 
-export default function TextMenu() {
-    return(
+interface TextLinksProps extends LinkProps {
+  texto: string;
+  iconElement?: React.ReactNode;
+}
 
-        <>
-        
-        
-        </>
-    );
+export default function TextMenu({
+  texto,
+  href,
+  iconElement,
+  ...rest
+}: TextLinksProps) {
+  return (
+    <>
+      <Link href={href} {...rest} className="no-underline">
+        <div className="flex items-center text-2xl text-white no-underline text-left hover:text-gray-400 hover:text-primary">
+          {iconElement && <span className="mr-2">{iconElement}</span>}
+          {texto}
+        </div>
+      </Link>
+    </>
+  );
 }
--- app/components/molecules/header/Header.tsx
+++ app/components/molecules/header/Header.tsx
@@ -2,23 +2,28 @@ import React from "react";
 import Image from "next/image";
 import TitleHeader from "../../atoms/titleHeader/TitleHeader";
 import Fundo from "../../../../public/wall_top.jpg";
+import NavBar from "../navBar/NavBar";
 
 export default function Header() {
   return (
-    <header className="relative h-[550px] flex items-center justify-center text-center">
-      {" "}
-      {/* text-center para o conteúdo */}
-      <Image
-        src={Fundo}
-        alt="Imagem de fundo do cabeçalho"
-        layout="fill"
-        objectFit="cover"
-        priority={true}
-      />
-      <div className="relative z-10 p-4">
-        <TitleHeader title="Star Wars" subtitle="Que a Força esteja com você" />
-        {/*  outras coisas aqui */}
-      </div>
-    </header>
+    <>
+    <NavBar />
+      <header className="relative h-[550px] flex items-center justify-center text-center">
+        <Image
+          src={Fundo}
+          alt="Imagem de fundo do cabeçalho"
+          layout="fill"
+          objectFit="cover"
+          priority={true}
+        />
+        <div className="relative z-10 p-4">
+          <TitleHeader
+            title="Star Wars"
+            subtitle="Que a Força esteja com você"
+          />
+          {/*  outras coisas aqui */}
+        </div>
+      </header>
+    </>
   );
 }
