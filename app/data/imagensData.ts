1d1521286f8b8b922bb44b1293291311d86b4915
CristinaJung <cristijung@gmail.com>
2025-05-19 15:37:21 -0300

    galeria de imagens feita com context ok

--- app/data/imagensData.ts
+++ app/data/imagensData.ts
@@ -5,25 +5,25 @@ import { ImageItem } from '../types/interfaces/interfaces';
 export const mockImages: ImageItem[] = [
   {
     id: '1',
-    url: './wall1.jpg',
+    url: '/wall1.jpg',
     title: 'Star Wars',
     description: 'A melhor história',
   },
   {
     id: '2',
-    url: './wall2.jpg',
+    url: '/wall2.jpg',
     title: 'Star Wars',
     description: 'A melhors história',
   },
   {
     id: '3',
-    url: './wall3.jpg',
+    url: '/wall3.jpg',
     title: 'Star Wars',
     description: 'A melhor história',
   },
   {
     id: '4',
-    url: './wall4.jpg',
+    url: '/wall4.jpg',
     title: 'Star Wars',
     description: 'A melhor história',
   },
--- app/wallpapers/page.tsx
+++ app/wallpapers/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-//import { ListaCards } from "../components/molecules/cards/ListCards";
+import { ListaCards } from "../components/molecules/cards/ListCards";
+import { mockImages } from "../data/imagensData";
 
 export default function WallPapers() {
-    return(
-        <>
-        Lista das imagens
-        
-        
-        </>
-    );
+  const imagesToDisplay = mockImages;
+  return (
+    <>
+      <h1 className="text-2xl font-bold my-4 text-center">
+        Galeria de Wallpapers
+      </h1>
+
+      <ListaCards items={imagesToDisplay} />
+    </>
+  );
 }
